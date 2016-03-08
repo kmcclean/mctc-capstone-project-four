a0e94c117160ea41ddf91f1eb47745f77e12477f
KMcClean <mcclean@gmail.com>
2016-03-08 11:16:15 -0600

    NPR Running. Working on OpenSecrets

--- open_secrets/core/DBViewer.py
+++ open_secrets/core/DBViewer.py
@@ -1,5 +1,6 @@
 from open_secrets.core.DBController import DBController
 from urllib import request
+from open_secrets.third_party_lib.votesmart import votesmart
 import json
 
 
@@ -34,37 +35,54 @@ class DBViewer:
         new_term = search_term.replace(" ", new_phrase)
         return new_term
 
+    #This gets stories from NPR related to the search subject.
     def get_npr(self, search_list):
         try:
             npr_key = self.get_key("NPR_API_KEY")
             for term in search_list:
+
+                #this is needed to process the API requests properly.
                 new_term = self.search_term_converter(term, "%20")
                 url = "http://api.npr.org/query?id=1014&fields=title,relatedLink&searchTerm=" + new_term + "&dateType=story&output=JSON&apiKey=" + npr_key
                 npr_stories = request.urlopen(url).read().decode('utf-8')
                 npr_json = json.loads(npr_stories)
 
-                npr_json_level_one = npr_json["list"]
-                npr_json_level_two = npr_json_level_one["story"]
-                for story in npr_json_level_two:
+                # NPR uses a different structure to their JSON output when there are no stories. This try block catches...
+                # situations where no stories are returned.
+                try:
+                    npr_json_level_one = npr_json["list"]
+                    npr_json_level_two = npr_json_level_one["story"]
+                    for story in npr_json_level_two:
 
-                    npr_json_level_three= story["title"]
-                    print(npr_json_level_three.get("$text"))
+                        npr_json_level_three= story["title"]
+                        print(npr_json_level_three.get("$text"))
 
-                    npr_json_level_four = story["link"]
-                    npr_json_level_five = npr_json_level_four[0]
-                    npr_json_level_six = npr_json_level_five["$text"]
-                    print(npr_json_level_six)
+                        npr_json_level_four = story["link"]
+                        npr_json_level_five = npr_json_level_four[0]
+                        npr_json_level_six = npr_json_level_five["$text"]
+                        print(npr_json_level_six)
+
+                # if NPR does not have any stories on the subject, they produce a different form of API results. This catches that lack of stories
+                except KeyError:
+                    print("NPR has no stories on this subject.")
 
         except Exception as e:
             print("Error in get_npr: " + e)
+
+    def get_vote_smart(self):
+        votesmart.apikey = self.get_key("Vote_Smart_API_KEY")
+        print(votesmart.apikey)
+        address = votesmart.address.getOffice(26732)[0]
+        print(address.city)
+
 
     #this tries to get the open secrets json files for the candidates that have been selected.
     def get_open_secrets(self):
         search_results = self.search()
         api_key = self.get_key("Open_Secrets_API_KEY_2")
         results_list = []
-        for item in search_results:
 
+        for item in search_results:
             url = "http://www.opensecrets.org/api/?method=candContrib&cid=" + item[0] + "&cycle=2016&apikey=" + api_key +  "&output=json"
             r = request.Request(url, data=None, headers={'User-Agent': 'Mozilla/5.0'})
             open_secrets_results = request.urlopen(r).read().decode('utf-8')
@@ -121,7 +139,8 @@ class DBViewer:
 
 
 view = DBViewer()
-top_results = view.get_open_secrets()
-org_results = view.output_open_secrets_results(top_results)
-view.get_npr(org_results)
+# top_results = view.get_open_secrets()
+# org_results = view.output_open_secrets_results(top_results)
+# view.get_npr(org_results)
+view.get_vote_smart()
 view.close()
